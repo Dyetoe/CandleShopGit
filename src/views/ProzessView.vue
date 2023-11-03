faa848de0e5ce6a7517da6c815fc9ad27ffeafe7
Lennart12345678 <lennart.elsaesser@bbs1-lueneburg.de>
2023-11-03 16:04:37 +0100

    Added content to ProzessView

--- src/views/ProzessView.vue
+++ src/views/ProzessView.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="prozess">
-    <h1>Das ist die Seite für den Prozess</h1>
+    <h1 class="pt-2 pb-2">Prozess</h1>
+    <div class="container-fluid">
+      <div class="row row-cols-*">
+        <div class="col-lg-4">
+          <img class="img-placeholder">
+        </div>
+        <div class="col-lg-4 pt-4 pt-lg-0 pb-4 pb-lg-0">
+          <div class="prozess-text">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <img class="img-placeholder">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .prozess {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+
+.img-placeholder{
+  background-color: #939393;
+  width: 100%;
+  min-width: 30%;
+  height: 20rem;
 }
+
 </style>
+<script setup>
+</script>
