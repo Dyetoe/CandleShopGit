646c6ffbca824aa4d37c09c026fb09e76cc7200b
s.hinrichs <s.hinrichs@wnm.de>
2023-11-03 12:15:21 +0100

    add HeaderComponent, add import Bootstrap main.js, removed needless css

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="wrapper p-2">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img alt="Vue logo" class="logo" src="@/assets/logo_candleshop.png" width="100" height="100" />
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/">Home</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/prozess">Prozess</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/kontakt">Kontakt</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/impressum">Impressum</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+<script setup>
+
+import {RouterLink} from "vue-router";
+</script>
+
+<style scoped>
+nav {
+  width: 100%;
+  text-align: right;
+}
+
+nav a.router-link-exact-active {
+  text-decoration: none;
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  color: #282828;
+  font-size: 25px;
+  text-decoration: none;
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+</style>
