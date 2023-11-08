7dfb0489889ee2092326650aa082525f04db0758
Dyetoe <emil.gietschel@bbs1-lueneburg.de>
2023-11-08 11:46:50 +0100

    Kontaktformular hinzugefügt (noch keine logik hinter)

--- src/views/KontaktView.vue
+++ src/views/KontaktView.vue
@@ -1,8 +1,39 @@
 <template>
-  <div class="kontakt">
-    <h1>Das ist die Seite für das Kontaktformular</h1>
+  <div>
+    <h1>Kontaktformular</h1>
+    <label for="email">E-Mail:</label>
+    <div class="form-group">
+      
+      <input type="email" id="email" v-model="email" required>
+    </div>
+    <label for="comment">Kommentar:</label>
+    <div class="form-group">
+      
+      <textarea id="comment" v-model="comment" required></textarea>
+    </div>
+    <div class="form-group">
+      <br>
+      <button @click="submitForm">Abschicken</button>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      email: "",
+      comment: ""
+    };
+  },
+  methods: {
+    submitForm() {
+    }
+  }
+});
+</script>
 
 <style>
 @media (min-width: 1024px) {
