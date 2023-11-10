e3fd3203dc7182f8181451b0f23e315c5e760c82
Dyetoe <emil.gietschel@bbs1-lueneburg.de>
2023-11-10 08:31:50 +0100

    added a very fancy button

--- src/views/KontaktView.vue
+++ src/views/KontaktView.vue
@@ -13,7 +13,7 @@
     </div>
     <div class="form-group">
       <br>
-      <button @click="submitForm">Abschicken</button>
+      <button class="button-62" @click="submitForm">Abschicken</button>
     </div>
   </div>
 </template>
@@ -42,5 +42,35 @@ export default defineComponent({
     display: flex;
     align-items: center;
   }
+}
+/* CSS */
+.button-62 {
+  background: linear-gradient(to bottom right, #EF4765, #FF9A5A);
+  border: 0;
+  border-radius: 12px;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: -apple-system,system-ui,"Segoe UI",Roboto,Helvetica,Arial,sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 2.5;
+  outline: transparent;
+  padding: 0 1rem;
+  text-align: center;
+  text-decoration: none;
+  transition: box-shadow .2s ease-in-out;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+}
+
+.button-62:not([disabled]):focus {
+  box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
+}
+
+.button-62:not([disabled]):hover {
+  box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
 }
 </style>
