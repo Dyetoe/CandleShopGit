d4b9135982a772369e2f3edf809784d30b0ec7d4
Lennart12345678 <lennart.elsaesser@bbs1-lueneburg.de>
2023-11-17 11:38:33 +0100

    Added margin on prozess images and added block sections fro text

--- src/views/ProzessView.vue
+++ src/views/ProzessView.vue
@@ -4,19 +4,27 @@
     <div class="container-fluid">
       <div class="row row-cols-*">
         <div class="col-lg-4">
-          <img class="prozessimg" src="@/assets/Prozess1.jpg" width="100%">
+          <img class="prozessimg mb-1" src="@/assets/Prozess1.jpg" width="100%">
           <img class="prozessimg" src="@/assets/Prozess3.jpg" width="100%">
         </div>
         <div class="col-lg-4 pt-4 pt-lg-0 pb-4 pb-lg-0">
           <div class="prozess-text">
-           Wir, die Mitglieder des Schülerunternehmens „Candleshop“, bestehen aus 14 engagierten Schülerinnen und Schülern der HA23B und nehmen unsere Tätigkeit mit Stolz im Rahmen unserer einjährigen Berufsfachschule Wirtschaft mit Schwerpunkt Handel an den Berufsbildenden Schulen I in Lüneburg wahr.
-Unser Hauptziel ist die Produktion und der Verkauf hochwertiger Kerzen, die an unserem Verkaufsstand in der Schule erhältlich sind. Unser einzigartiges Konzept hebt sich hervor, da wir sowohl regionales Bienenwachs als auch Sojawachs nutzen, um eine breite Palette von Kerzen anzubieten. Damit können wir auch veganen Kunden gerecht werden, die unsere Kerzen schätzen. Die Verwendung von lokal beschafftem Bienenwachs stärkt die Verbindung zur Region und unterstützt gleichzeitig Imker vor Ort.
-Wir laden Sie herzlich dazu ein, unsere Kerzen und unsere Philosophie während des anstehenden Winterzaubers in der Pausenhalle der BBS I Lüneburg am 05. Dezember 2023 zu erleben.
-Wir sind überzeugt, dass Sie von der Qualität unserer Kerzen und dem Engagement unseres Schülerunternehmens beeindruckt sein werden. Mit Ihrem Kauf unterstützen Sie nicht nur unsere Bildung, sondern tragen auch zur Förderung nachhaltiger und regionaler Produkte bei.
+            <p>
+              Wir, die Mitglieder des Schülerunternehmens „Candleshop“, bestehen aus 14 engagierten Schülerinnen und Schülern der HA23B und nehmen unsere Tätigkeit mit Stolz im Rahmen unserer einjährigen Berufsfachschule Wirtschaft mit Schwerpunkt Handel an den Berufsbildenden Schulen I in Lüneburg wahr.
+            </p>
+            <p>
+              Unser Hauptziel ist die Produktion und der Verkauf hochwertiger Kerzen, die an unserem Verkaufsstand in der Schule erhältlich sind. Unser einzigartiges Konzept hebt sich hervor, da wir sowohl regionales Bienenwachs als auch Sojawachs nutzen, um eine breite Palette von Kerzen anzubieten. Damit können wir auch veganen Kunden gerecht werden, die unsere Kerzen schätzen. Die Verwendung von lokal beschafftem Bienenwachs stärkt die Verbindung zur Region und unterstützt gleichzeitig Imker vor Ort.
+            </p>
+            <p>
+              Wir laden Sie herzlich dazu ein, unsere Kerzen und unsere Philosophie während des anstehenden Winterzaubers in der Pausenhalle der BBS I Lüneburg am 05. Dezember 2023 zu erleben.
+            </p>
+            <p>
+              Wir sind überzeugt, dass Sie von der Qualität unserer Kerzen und dem Engagement unseres Schülerunternehmens beeindruckt sein werden. Mit Ihrem Kauf unterstützen Sie nicht nur unsere Bildung, sondern tragen auch zur Förderung nachhaltiger und regionaler Produkte bei.
+            </p>
           </div>
         </div>
         <div class="col-lg-4">
-          <img class="prozessimg" src="@/assets/Prozess2.jpg" width="100%">
+          <img class="prozessimg mb-1" src="@/assets/Prozess2.jpg" width="100%">
           <img class="prozessimg" src="@/assets/Prozess4.jpeg" width="100%">
         </div>
       </div>
